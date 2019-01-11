be0bc9b715283231c2128fe06ab40492ac41122b
Linko-3D <dbekhouc@gmail.com>
2019-01-12 00:48:57 +0100

    fix

--- __init__.py
+++ __init__.py
@@ -18,12 +18,6 @@ from . import GA
 
 class GA_Props(bpy.types.PropertyGroup):
 
-   ga_autoexportglb : bpy.props.BoolProperty(
-        name = 'Auto Export .glb',
-        description = "After generating your game asset, the addon will automatically export a .glb (it will contain all your baked textures). Set the path to the asset folder of your game project",
-        default = True
-   )
-   
    ga_pathglb : bpy.props.StringProperty(
         name="Path", 
         description = "Choose where to save your file",
