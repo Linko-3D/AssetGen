6161a0835a89cc1f3686e4f355720e9dbe8d35f6
Linko-3D <dbekhouc@gmail.com>
2019-01-12 15:21:10 +0100

    tweak

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ class GA_Props(bpy.types.PropertyGroup):
 
    ga_pathglb : bpy.props.StringProperty(
         name="Path", 
-        description = "Choose where to save the .glb file",
+        description = "Indicate the path to your game asset folder to save the .glb file",
         default='//',
         subtype = 'DIR_PATH'
    )   
