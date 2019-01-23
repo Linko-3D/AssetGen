83b6a951490b163e9f67f477cb482b1cb70eb4b8
Linko-3D <dbekhouc@gmail.com>
2019-01-23 16:37:27 +0100

    tweak union boolean

--- GA.py
+++ GA.py
@@ -220,6 +220,7 @@ class GA_Start(bpy.types.Operator):
 
 				bpy.ops.mesh.select_non_manifold()
 				bpy.ops.mesh.edge_face_add()
+				bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
 
 				bpy.ops.mesh.select_all(action = 'SELECT')
 
