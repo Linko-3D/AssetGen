05eec0ffc8e5e2a01f8a3b9eee4e1bbb052bf7a3
Linko <dbekhouc@gmail.com>
2019-05-04 12:51:13 +0200

    polish tool added

--- GA_user_interface.py
+++ GA_user_interface.py
@@ -181,7 +181,8 @@ class GA_toolsPanel(bpy.types.Panel):
         col1_ga = layout.column(align=True)
         col1_ga.scale_y = 1.0
         col1_ga.operator("scene.ga_toolshighpoly", icon="FILE_REFRESH")
-        col1_ga.operator("scene.ga_toolswear", icon="FILE_REFRESH")		
+        col1_ga.operator("scene.ga_toolpolish", icon="FILE_REFRESH")
+        col1_ga.operator("scene.ga_toolswear", icon="FILE_REFRESH")			
 
 		#-----------------------------------------------------------	
         col_ga = layout.column(align=True)		
