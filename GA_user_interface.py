0eefe5d65e6b7c25adafe33d44a38040f751b562
Linko-3D <dbekhouc@gmail.com>
2019-05-10 15:17:25 +0200

    tweak

--- GA_user_interface.py
+++ GA_user_interface.py
@@ -130,14 +130,6 @@ class GA_advancedPanel(bpy.types.Panel):
         row.prop(myscene , "ga_smoothLP")
         col_ga.row().separator()
 
-        row = col_ga.row()		
-        row.prop(myscene , "ga_centerXY")
-        col_ga.row().separator()
-		
-        row = col_ga.row()		
-        row.prop(myscene , "ga_ontheground")
-        col_ga.row().separator()	
-
         col_ga.prop(myscene , 'ga_cagesize') 
         col_ga.prop(myscene , 'ga_edgepadding') 
         col_ga.prop(myscene , 'ga_uvmargin') 
@@ -154,7 +146,15 @@ class GA_advancedPanel(bpy.types.Panel):
 
         row = col_ga.row()		
         row.prop(myscene , "ga_removeunderground")
+        col_ga.row().separator()		
+
+        row = col_ga.row()		
+        row.prop(myscene , "ga_centerXY")
         col_ga.row().separator()
+		
+        row = col_ga.row()		
+        row.prop(myscene , "ga_ontheground")
+        col_ga.row().separator()	
 		
         col_ga.row().separator()		
 		
