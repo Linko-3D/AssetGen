e7d157469ad0d1102f2de11d7a84d0101ebc7adc
Linko-3D <dbekhouc@gmail.com>
2019-05-13 19:57:02 +0200

    tweak

--- GA_tools.py
+++ GA_tools.py
@@ -875,7 +875,7 @@ class GA_Tools_StrapLine(bpy.types.Operator):
 		bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
 
 		bpy.ops.object.mode_set(mode = 'EDIT')
-		bpy.ops.curve.handle_type_set(type='ALIGNED')
+		bpy.ops.transform.resize(value=(0.5, 0.5, 0.5), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
 		bpy.ops.object.mode_set(mode = 'OBJECT')
 		
 		bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
